e768ebfd9e6108ec26257963a7d622c1a999bc1b
Belete Misgan Mengie <154594574+BeleteGM@users.noreply.github.com>
2024-03-29 03:49:00 -0900

    first attached

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "Belete";
+const meta = {
+    title: "Belete MISGAN",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Belete Misgan",
+    animated: {
+        first: "I am MERN stack website developer",
+        second: "I love coding",
+        third: "I code cool websites",
+        fourth:"I am Network Engineer",
+        fifth: "I am video editer"
+
+    },
+    description: "I'm MERN stack website developer, I specialize in developing web applications using the MERN stack, which consists of MongoDB &MYSQL, Express.js, React.js, and Node.js. This powerful combination of technologies allows you to create dynamic, responsive, and scalable web applications.",
+    your_img_url: "https://t3.ftcdn.net/jpg/04/44/20/52/240_F_444205283_KPt29FANU6TWXBgDHd0KL6zVkYbDUfua.jpg",
+};
+
+const dataabout = {
+    title: "Belete Misgan",
+    aboutme: "I have BSC computer scince  Degree from Hawassa University and  I am a skilled MERN stack website developer with a passion for creating exceptional web applications. With expertise in MongoDB, Express.js, React.js, and Node.js, I specialize in delivering seamless user experiences through intuitive interfaces and robust back-end development. I stay updated with the latest web development trends and technologies to provide modern, scalable, and future-proof solutions. With a focus on client satisfaction, I bring professionalism, problem-solving skills, and effective communication to every project. Let's collaborate and transform your ideas into powerful and dynamic web applications.",
+};
+
+
+const skills = [{
+        name: "MongoDB and MYSQL",
+    
+    },
+    {
+        name: "express",
+        
+    },
+{
+    name: "css, tailwind and bootstrap",
+    
+},
+
+    {
+        name: "react.js, jquery and javascript",
+        
+    },
+    
+    {
+        name: "nodejs",
+       
+    },
+    
+    
+    {
+        name: "video editing",
+       
+    },
+    {
+        name: "Network Engineer",
+        
+    },
+];
+
+
+const services = [{
+        title: "website Development",
+        description: "We provide custom MERN stack web development services, crafting powerful and scalable web applications. Our expertise in UI/UX design, restfull APIs, and deployment ensures exceptional user experiences. Let us bring your ideas to life and drive your business forward with our MERN stack expertise. Contact us to discuss your project requirements today.",
+    },
+    {
+        title: "Teach Mern stack website development",
+        description: "I offer expert MERN stack website development teaching services. Learn to build dynamic web applications using MongoDB, Express.js, React.js, and Node.js. Gain practical skills and hands-on experience from our experienced instructors. Enhance your web development journey with our comprehensive MERN stack teaching program.",
+    },
+    {
+        title: "Video Editing",
+        description: "We provide professional video editing services tailored to your needs. Our skilled editors transform raw footage into polished, engaging videos. From corporate videos to social media content, we ensure high-quality edits, seamless transitions, and captivating visuals. Let us bring your vision to life with our expert video editing services.",
+    },
+    {
+        title: "Network Engineer",
+        description: "I offer comprehensive network engineering services to optimize and secure your network infrastructure. Our experienced engineers provide expert solutions in network design, implementation, troubleshooting, and maintenance. Enhance performance, reliability, and security with our tailored network engineering services for your business.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "gmnetwork21@gmail.com",
+    YOUR_FONE: "(+251)919393210",
+    description: "I provide efficient contact me services. Reach out to us today to discuss your needs and how we can assist you.. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/BeleteGM/",
+    facebook: "https://www.facebook.com/profile.php?id=61554593069269",
+    linkedin: "https://www.linkedin.com/in/belete21/",
+    youtube: "https://www.youtube.com/channel/UCA05BbF3UlYXQ38XEc8pQLA",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
